4f32768607f06f9c3d2f4290f055a781ef3fa4b2
darryl <darryl.zaidra@gmail.com>
2023-12-29 22:07:48 +0700

    u

--- utils/db.js
+++ utils/db.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/finalterm')
+mongoose.connect('mongodb+srv://darrylnwfal:v2H808TyB6C0lnv1@cluster0.ekuav4x.mongodb.net/finalterm')
